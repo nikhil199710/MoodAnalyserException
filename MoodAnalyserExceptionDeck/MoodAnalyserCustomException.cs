5ae2eae3bfac62a1f65ee7c2d7a24e8b2e3a6129
Nikhil Kumar Yadav <nikhilyadavindigo@gmail.com>
2020-10-13 03:45:19 +0530

    UC3_EmptyNull_Message

---
+++ MoodAnalyserExceptionDeck/MoodAnalyserCustomException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyserExceptionDeck
+{
+    public class MoodAnalyserCustomException:Exception
+    {
+        public enum Exceptiontypes
+        {
+            Empty_Message,Null_Message 
+        }
+
+        public readonly Exceptiontypes type;
+        
+        public MoodAnalyserCustomException(Exceptiontypes type,string message):base(message)
+        {
+            this.type = type;
+        }
+
+    }
+}
